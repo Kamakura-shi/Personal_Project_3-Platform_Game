e4ed5a22b61603949a43698f25de762e3c64c13c
Hani Ouali <hani.ouali.1@ens.etsmtl.ca>
2023-09-25 18:17:57 -0400

    Flappy Game can run, bird and other objects are fully functional. Added start and exit buttons

--- tutorial.py
+++ tutorial.py
@@ -3,21 +3,43 @@ import neat
 import time
 import os
 import random
+pygame.font.init()
 
 WIN_WIDTH = 500
 WIN_HEIGHT = 800
+JUMP_VEL = -10.5
 
+screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 
-BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png")))]
+start_img = pygame.image.load('start_btn.png').convert_alpha()
+exit_img = pygame.image.load('exit_btn.png').convert_alpha()
+
+class Button():
+	def __init__(self, x, y, image):
+			self.image = image
+			self.rect = self.image.get_rect()
+			self.rect.topleft = (x, y)
+	
+	def draw(self):
+		screen.blit(self.image, (self.rect.x, self.rect.y))
+
+start_button = Button(110, 200, start_img)
+exit_button = Button(130, 450, exit_img)
+
+BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
 PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+
+STAT_FONT = pygame.font.SysFont("comicsans", 50)
+
 
 class Bird:
 	IMGS = BIRD_IMGS
 	MAX_ROTATION = 25
 	ROT_VEL = 20
 	ANIMATION_TIME = 5
+	GRAVITY = 1
 
 	def __init__(self, x, y):
 		self.x = x
@@ -26,16 +48,19 @@ class Bird:
 		self.tick_count = 0
 		self.vel = 0
 		self.height = self.y
-		self.image_count = 0
+		self.img_count = 0
 		self.img = self.IMGS[0]
 
 	def jump(self):
-		self.vl = -10.5
+		self.vel = JUMP_VEL
 		self.tick_count = 0
 		self.height = self.y
 
 	def move(self):
 		self.tick_count += 1
+
+		self.vel += self.GRAVITY
+		self.y += self.vel
 
 		d = self.vel*self.tick_count + 1.5*self.tick_count**2
 
@@ -58,49 +83,182 @@ class Bird:
 		self.img_count += 1
 
 		if self.img_count < self.ANIMATION_TIME:
-			self.img = self.IMG[0]
+			self.img = self.IMGS[0]
 		elif self.img_count < self.ANIMATION_TIME*2:
-			self.img = self.IMG[1]
+			self.img = self.IMGS[1]
 		elif self.img_count < self.ANIMATION_TIME*3:
-			self.img = self.IMG[2]
+			self.img = self.IMGS[2]
 		elif self.img_count < self.ANIMATION_TIME*4:
-			self.img = self.IMG[1]
+			self.img = self.IMGS[1]
 		elif self.img_count < self.ANIMATION_TIME*4 + 1:
-			self.img = self.IMG[0]
-			self.img = 0
+			self.img = self.IMGS[0]
+			self.img_count = 0
 
 		if self.tilt <= -80:
 			self.img = self.IMGS[1]
 			self.img_count = self.ANIMATION_TIME*2
 
 		rotated_image = pygame.transform.rotate(self.img, self.tilt)
-		new_rect = rotated_image.get_rect(center=self.img.get_rect(topLeft = (self.x, self.y)).center)
+		new_rect = rotated_image.get_rect(center=self.img.get_rect(topleft = (self.x, self.y)).center)
 		win.blit(rotated_image, new_rect.topleft)
 
 	def get_mask(self):
 		return pygame.mask.from_surface(self.img)
 
-def draw_window(win, bird):
+class Pipe:
+	GAP = 200
+	VEL = 5
+
+	def __init__(self, x):
+		self.x = x
+		self.height = 0
+
+		self.top = 0
+		self.bottom = 0
+		self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
+		self.PIPE_BOTTOM = PIPE_IMG
+
+		self.passed = False
+		self.set_height()
+
+	def set_height(self):
+		self.height = random.randrange(50, 450)
+		self.top = self.height - self.PIPE_TOP.get_height()
+		self.bottom = self.height + self.GAP
+
+	def move(self):
+		self.x -= self.VEL
+
+	def draw(self, win):
+		win.blit(self.PIPE_TOP, (self.x, self.top))
+		win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+
+	def collide(self, bird):
+		bird_mask = bird.get_mask()
+		top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+		bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+
+		top_offset = (self.x - bird.x, self.top - round(bird.y))
+		bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+		b_point = bird_mask.overlap(bottom_mask, bottom_offset)
+		t_point = bird_mask.overlap(top_mask, top_offset)
+
+		if t_point or b_point:
+			return True
+
+		return False
+
+class Base:
+	VEL = 5
+	WIDTH = BASE_IMG.get_width()
+	IMG = BASE_IMG
+
+	def __init__(self, y):
+		self.y = y
+		self.x1 = 0
+		self.x2 = self.WIDTH
+
+	def move(self):
+		self.x1 -= self.VEL
+		self.x2 -= self.VEL
+
+		if self.x1 + self.WIDTH < 0:
+			self.x1 = self.x2 + self.WIDTH
+
+		if self.x2 + self.WIDTH < 0:
+			self.x2 = self.x1 + self.WIDTH
+
+	def draw(self, win):
+			win.blit(self.IMG, (self.x1, self.y))
+			win.blit(self.IMG, (self.x2, self.y))
+
+def draw_window(win, bird, pipes, base, score):
 	win.blit(BG_IMG, (0,0))
+
+	for pipe in pipes:
+		pipe.draw(win)
+
+	text = STAT_FONT.render("Score: " + str(score), 1, (255, 255, 255))
+	win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
+
+	base.draw(win)
+
 	bird.draw(win)
 	pygame.display.update()
 
 def main():	
-	bird = Bird(200, 200)
+	bird = Bird(230, 350)
+	base = Base(730)
+	pipes = [Pipe(700)]
 	win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 	clock = pygame.time.Clock()
+	game_over = False
+
+	score = 0
 
 	run = True
 	while run:
+		
+		start_button.draw()
+		exit_button.draw()
+		
+		pygame.display.update()
+
 		clock.tick(30)
+		
 		for event in pygame.event.get():	
 			if event.type == pygame.QUIT:
 				run = False
 
-		bird.move()
-		draw_window(win, bird)
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_SPACE:
+				bird.jump()
+
+		#bird.move()
+		add_pipe = False
+		rem = []
+		for pipe in pipes:
+			if pipe.collide(bird):
+				game_over = True
+
+			if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+				rem.append(pipe)
+			
+			if not pipe.passed and pipe.x < bird.x:
+				pipe.passed = True
+				add_pipe = True
+			
+			pipe.move()
+		
+		if add_pipe:
+			score += 1
+			pipes.append(Pipe(600))
+
+		for r in rem:
+			pipes.remove(r)
+
+		#if bird.y + bird.img.get_height() > 730:
+			#game_over = True
+
+		base.move()
+		draw_window(win, bird, pipes, base, score)
 	
+	if game_over:
+			game_over_text = STAT_FONT.render("GAME OVER", 1, (255, 0, 0))
+			restart_text = STAT_FONT.render("Press SPACE to Restart", 1, (255, 255, 255))
+			win.blit(game_over_text, (WIN_WIDTH // 2 - game_over_text.get_width() // 2, WIN_HEIGHT // 2 - game_over_text.get_height() // 2))
+			win.blit(restart_text, (WIN_WIDTH // 2 - restart_text.get_width() // 2, WIN_HEIGHT // 2 + game_over_text.get_height()))
+
+			for event in pygame.event.get():
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_SPACE:
+						bird = Bird(230, 350)
+						pipes = [Pipe(700)]
+						score = 0
+						game_over = False
+
 	pygame.quit()
 	quit()
 
-main()				
+main()
